f727a693fea89a2b17f5ecd8fefa4674a1b1e128
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2026-04-03 22:10:43 +0000

    Update pages_fixed.dart

--- waste_justice/lib/pages_fixed.dart
+++ waste_justice/lib/pages_fixed.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show kIsWeb; 
  
 // location page for getting user location and selecting aggregators
 class LocationPage extends StatefulWidget {
